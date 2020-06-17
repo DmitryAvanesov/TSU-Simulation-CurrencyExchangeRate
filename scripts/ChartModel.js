ab9dd20ed3bb0703d3f60ac9977799e0ced13bc2
Dima <dima.avanesov@yandex.ru>
2020-06-17 22:27:36 +0700

    Added geometric brown motion

--- scripts/ChartModel.js
+++ scripts/ChartModel.js
@@ -6,6 +6,15 @@ class ChartModel {
     this.initialDate = new Date(2020, 2, 1);
     this.maxRateChange = 1.5;
     this.curExchangeRates = {};
+    this.drift = 1;
+    this.volatility = 0.01;
+    this.timeInterval = 0.0001;
+    this.wiener = {
+      USD: 0,
+      EUR: 0,
+      JPY: 0,
+      GBP: 0
+    };
   }
 
   generateData(numberOfDays, exchangeRates) {
@@ -16,7 +25,7 @@ class ChartModel {
 
       var tmpDate = new Date();
       tmpDate.setDate(this.initialDate.getDate() + curDay);
-  
+
       this.data.push({
         time: tmpDate.toLocaleString().split(',')[0],
         USD: this.curExchangeRates.USD,
@@ -28,8 +37,21 @@ class ChartModel {
   }
 
   randomizeExchangeRates() {
+    const standardNormalDistribution = () => {
+      const range = 6;
+      let value = 0;
+
+      for (let i = 0; i < range * 2; i++) {
+        value += Math.random();
+      }
+
+      value -= range;
+      return value;
+    };
+
     for (const rate in this.curExchangeRates) {
-      this.curExchangeRates[rate] = Math.max(0, (this.curExchangeRates[rate] + (Math.random() * (2 * this.maxRateChange) - this.maxRateChange)).toFixed(2));
+      this.wiener[rate] += Math.sqrt(this.timeInterval) * standardNormalDistribution();
+      this.curExchangeRates[rate] = Math.max(0, this.curExchangeRates[rate] * Math.exp((this.drift - this.volatility ** 2 / 2) * this.timeInterval + this.volatility * this.wiener[rate]));
     }
   }
 }
